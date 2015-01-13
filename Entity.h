5d8071c51872cf8b115ae185b97980494d7e313c
Nico Böckhoff <nico.boeckhoff@student.hpi.de>
2015-01-13 22:37:30 +0100

    Added movement options with hjkl keys.

--- Entity.h
+++ Entity.h
@@ -1,9 +1,16 @@
 #include "Terminal.h"
 
+enum Direction {
+		Up = 1,
+		Left = 2,
+		Down = 3,
+		Right = 4
+};
+
 class Entity {
 	public:
 	uint64_t posX, posY;
 	void render();
 	bool canEnter(uint64_t posX, uint64_t posY);
-	bool handleControl(uint8_t input);
+	bool moveControl(uint8_t input);
 };
